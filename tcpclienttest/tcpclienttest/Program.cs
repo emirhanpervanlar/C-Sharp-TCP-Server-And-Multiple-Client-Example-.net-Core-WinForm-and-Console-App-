30e17adc94d758522117ab08278448414013065a
Emirhan Pervanlar <pervanlaremirhan@gmail.com>
2020-07-10 00:48:31 +0300

    push

---
+++ tcpclienttest/tcpclienttest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpclienttest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            connectTCP();
+        }
+
+        static void connectTCP()
+        {
+            // Client oluşturuyoruz
+            using var client = new TcpClient();
+            client.Connect("127.0.0.1", 9999);
+
+            using NetworkStream networkStream = client.GetStream();
+
+            // Gelen mesajı okuyoruz
+            using var reader = new StreamReader(networkStream, Encoding.UTF8);
+            Byte[] bytes = new Byte[256];
+            String data = null;
+            try
+            {
+                int i;
+                while ((i = networkStream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+                    // Gelen dataya göre karşılaştırıp geriye cevap basıyoruz
+                    if(data == "10")
+                    {
+                        byte[] sendMsg = System.Text.Encoding.ASCII.GetBytes("false");
+                        networkStream.Write(sendMsg);
+                    }
+                    Console.WriteLine(data);
+                }
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            catch { }
+
+            //byte[] bytes = Encoding.UTF8.GetBytes(message);
+            //networkStream.Write(bytes, 0, bytes.Length);
+
+        }
+    }
+}
---
+++ tcpservertest/tcpservertest/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcpservertest
+{
+    public partial class Form1 : Form
+    {
+        // Bağlantıları kontrol etmek için listeye aldık
+        static List<NetworkStream> clients = new List<NetworkStream>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            startServer();
+        }
+
+        public void startServer()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+            TcpClient client;
+            server.Start();
+
+
+            while (true)  
+            {
+                try
+                {
+                    // Client oturumu bekliyoruz
+                    client = server.AcceptTcpClient();
+
+                    // Birden fazla oturum dinleyebilmek için thread ile kullanıyoruz
+                    ThreadPool.QueueUserWorkItem(ThreadProc, client);
+                }
+                catch { }
+            }
+        }
+
+        private void ThreadProc(object obj)
+        {
+            try
+            {
+                // Gelen mesajları işlemek için varsayılan değerleri oluşturuyoruz
+                Byte[] bytes = new Byte[256];
+                String data = null;
+                // Oturumu çekiyoruz
+                var client = (TcpClient)obj;
+
+                // Oturumdan gelen mesajı okuyoruz
+                NetworkStream ns = client.GetStream();
+                byte[] msg2 = System.Text.Encoding.ASCII.GetBytes("10");
+
+                // Clientımızı havumuza ekliyoruz
+                clients.Add(ns);
+
+                // Client'a mesaj gönderiyoruz
+                ns.Write(msg2, 0, msg2.Length);
+
+                // Verileri okuyoruz
+                int i;
+                while ((i = ns.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    MessageBox.Show(data);
+                }
+            }
+            catch { }
+        }
+
+    }
+}
